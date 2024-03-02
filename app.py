18fefef8ec90853400987c1785393650db48e92d
unknown <rafiafridi783@gmail.com>
2024-03-02 10:05:00 -0800

    code share

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import glob
+import os
 
 def main():
     st.title("Jugar Codeshare")
@@ -13,6 +14,15 @@ def main():
     # Password input field
     password = st.text_input("Enter Password", type="password")
     
+    # Upload file functionality
+    uploaded_file = st.file_uploader("Upload a file", type=["txt", "ipynb", "md", "py", "sql"])
+
+    if uploaded_file:
+        # Save the uploaded file to the data folder with the same extension
+        with open(os.path.join("data", uploaded_file.name), "wb") as f:
+            f.write(uploaded_file.getvalue())
+        st.success("File uploaded successfully!")
+        
     if password == "sleep":
         if selected_file:
             # Read the contents of the selected file
@@ -23,6 +33,14 @@ def main():
             st.text_area("File Content", file_contents, height=1000)
     else:
         st.write("ENTER Password to show data")
-
+    
+    # Delete file functionality
+    if st.button("Delete File"):
+        try:
+            os.remove(os.path.join(data_folder, selected_file))
+            st.success(f"File '{selected_file}' deleted successfully!")
+        except Exception as e:
+            st.error(f"Error deleting file: {e}")
+            
 if __name__ == "__main__":
     main()
